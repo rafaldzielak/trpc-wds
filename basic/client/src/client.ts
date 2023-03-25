36a9edf1fa66b64cb358d224e090504435b5f7e3
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-03-25 15:52:33 +0100

    BE & FE: Validation with zod

--- basic/client/src/client.ts
+++ basic/client/src/client.ts
@@ -12,6 +12,12 @@ const client = createTRPCProxyClient<AppRouter>({
 async function main() {
   const result = await client.sayHi.query();
   console.log(result);
+  const res = await client.logToServer.mutate("Hi from the client");
+  console.log(res);
+  const user = await client.users.get.query({ userId: "123" });
+  console.log(user);
+  const updatedUser = await client.users.update.mutate({ userId: "123", name: "Rafa" });
+  console.log(updatedUser);
 }
 
 main();
--- basic/server/routers/users.ts
+++ basic/server/routers/users.ts
@@ -1,12 +1,14 @@
 import { trpc } from "../trpc";
+import { z } from "zod";
 
-const userProcedure = trpc.procedure.input((v) => {
-  if (typeof v === "string") return v;
-  throw new Error("Invalid input: Expected string");
-});
+const userProcedure = trpc.procedure.input(z.object({ userId: z.string() }));
 
 export const userRouter = trpc.router({
-  getUser: userProcedure.query(() => {
-    return { id: 1, name: "Rafa" };
+  get: userProcedure.query(({ input }) => {
+    return { id: input.userId };
+  }),
+  update: userProcedure.input(z.object({ name: z.string() })).mutation(({ input }) => {
+    console.log(`Updating user: ${input.userId} to have name ${input.name}`);
+    return input;
   }),
 });
